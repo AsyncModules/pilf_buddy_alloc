78fc60c5f3b1783dda75bbfdf518e308db613c72
zflcs <1491657576@qq.com>
2025-04-20 11:45:57 +0800

    Cargo fmt & Remove warnings about var unused read.

--- src/linked_list/mod.rs
+++ src/linked_list/mod.rs
@@ -1,9 +1,6 @@
 /// 位置无关的无锁侵入式链表
-use crate::get_data_base;
-use core::marker::PhantomData;
-use core::{fmt, ptr};
-use node_ptr::{LinkedPtr, ListNode, NodePtr};
 pub(crate) use node_ptr::EMPTY_FLAG;
+use node_ptr::{LinkedPtr, ListNode, NodePtr};
 
 mod node_ptr;
 
@@ -13,9 +10,9 @@ mod node_ptr;
 ///
 /// Thanks Sergio Benitez for his excellent work,
 /// See [CS140e](https://cs140e.sergio.bz/) for more information
-/// 
+///
 /// 对该链表的无锁改造参考了论文[A Pragmatic Implementation of Non-Blocking Linked-Lists](https://timharris.uk/papers/2001-disc.pdf)
-/// 
+///
 /// 各个链表操作的参数和返回值都是实际地址。
 /// 将实际转换为地址无关地址的过程在链表内部完成。
 // #[derive(Copy, Clone)]
@@ -26,7 +23,6 @@ pub struct LinkedList {
 
 unsafe impl Send for LinkedList {}
 unsafe impl Sync for LinkedList {}
-
 
 impl LinkedList {
     /// Create a new LinkedList
@@ -49,7 +45,11 @@ impl LinkedList {
             let (left_node, right_node) = self.get_headptr_head();
             let new_node = NodePtr::from_value(item);
             new_node.pointed_node().set(right_node.linked_value());
-            if left_node.pointed_node().compare_exchange(right_node.linked_value(), new_node.linked_value()).is_ok() {
+            if left_node
+                .pointed_node()
+                .compare_exchange(right_node.linked_value(), new_node.linked_value())
+                .is_ok()
+            {
                 return;
             }
         }
@@ -57,9 +57,9 @@ impl LinkedList {
 
     /// Try to remove the first item in the list
     pub fn pop(&self) -> Option<*mut ()> {
-        let mut left_node = NodePtr::default();
-        let mut right_node = NodePtr::default();
-        let mut right_node_next = NodePtr::default();
+        let mut left_node;
+        let mut right_node;
+        let mut right_node_next;
         loop {
             // 查找与逻辑删除
             loop {
@@ -68,20 +68,31 @@ impl LinkedList {
                     return None;
                 }
                 right_node_next = right_node.next();
-                if !right_node_next.is_marked() { // 此处实际判断的是right_node节点是否被标记
-                    if right_node.pointed_node().compare_exchange(right_node_next.linked_value(), NodePtr::from_value(right_node_next.mark()).linked_value()).is_ok() {
+                if !right_node_next.is_marked() {
+                    // 此处实际判断的是right_node节点是否被标记
+                    if right_node
+                        .pointed_node()
+                        .compare_exchange(
+                            right_node_next.linked_value(),
+                            NodePtr::from_value(right_node_next.mark()).linked_value(),
+                        )
+                        .is_ok()
+                    {
                         break;
                     }
                 }
             }
             // 物理删除
-            if left_node.pointed_node().compare_exchange(right_node.linked_value(), right_node_next.linked_value()).is_err() {
+            if left_node
+                .pointed_node()
+                .compare_exchange(right_node.linked_value(), right_node_next.linked_value())
+                .is_err()
+            {
                 let (_, _) = self.search_with_ptr(right_node.value());
                 // 之后回到大循环，因为需要重新pop一项出来
-            }
-            else {
+            } else {
                 assert!(!right_node.is_marked());
-                return Some(right_node.value())
+                return Some(right_node.value());
             }
         }
     }
@@ -92,9 +103,9 @@ impl LinkedList {
     /// 返回值true代表链表中有所找项并成功删除；false代表没有所找项。
     /// 不会出现链表中有所找项但删除失败的情况。
     pub fn delete(&self, item: *mut ()) -> bool {
-        let mut left_node = NodePtr::default();
-        let mut right_node = NodePtr::default();
-        let mut right_node_next = NodePtr::default();
+        let mut left_node;
+        let mut right_node;
+        let mut right_node_next;
 
         // 查找与逻辑删除
         loop {
@@ -103,14 +114,26 @@ impl LinkedList {
                 return false;
             }
             right_node_next = right_node.next();
-            if !right_node_next.is_marked() { // 此处实际判断的是right_node节点是否被标记
-                if right_node.pointed_node().compare_exchange(right_node_next.linked_value(), NodePtr::from_value(right_node_next.mark()).linked_value()).is_ok() {
+            if !right_node_next.is_marked() {
+                // 此处实际判断的是right_node节点是否被标记
+                if right_node
+                    .pointed_node()
+                    .compare_exchange(
+                        right_node_next.linked_value(),
+                        NodePtr::from_value(right_node_next.mark()).linked_value(),
+                    )
+                    .is_ok()
+                {
                     break;
                 }
             }
         }
         // 物理删除
-        if left_node.pointed_node().compare_exchange(right_node.linked_value(), right_node_next.linked_value()).is_err() {
+        if left_node
+            .pointed_node()
+            .compare_exchange(right_node.linked_value(), right_node_next.linked_value())
+            .is_err()
+        {
             let (_, _) = self.search_with_ptr(right_node.value());
         }
         return true;
@@ -119,65 +142,17 @@ impl LinkedList {
 
 // private函数
 impl LinkedList {
-    fn search_with_ptr(&self, item: *mut ()) -> (NodePtr, NodePtr) { // 两个返回值分别为left_node和right_node
+    fn search_with_ptr(&self, item: *mut ()) -> (NodePtr, NodePtr) {
+        // 两个返回值分别为left_node和right_node
         let mut left_node: NodePtr = NodePtr::default();
         let mut left_node_next: NodePtr = NodePtr::default();
-        let mut right_node: NodePtr = NodePtr::default();
+        let mut right_node: NodePtr;
         loop {
             loop {
-                let mut t: NodePtr = NodePtr::from_value(&self.head as *const ListNode as *mut ListNode as *mut ());
+                let mut t: NodePtr =
+                    NodePtr::from_value(&self.head as *const ListNode as *mut ListNode as *mut ());
                 let mut t_next: NodePtr = self.head.marked_ptr();
-    
-                /* 1: Find left_node and right_node */
-                loop {
-                    if !t_next.is_marked() {
-                        left_node = t;
-                        left_node_next = t_next;
-                    }
-                    t = NodePtr::from_value(t_next.unmark());
-                    if t.value() == EMPTY_FLAG {
-                        break;
-                    } 
-                    t_next = t.next();
-                    // rust没有do-while，因此这样退出循环
-                    if !(t_next.is_marked() || (t.ptr() != item)) { // if的第二个条件，将原论文的按值查找改为了按指针查找
-                        break;
-                    }
-                }
-                right_node = t;
-    
-                /* 2: Check nodes are adjacent*/
-                if left_node_next.value() == right_node.value() {
-                    if (right_node.value() != EMPTY_FLAG) && right_node.pointed_node().is_marked() {
-                        break;
-                    }
-                    else {
-                        return (left_node, right_node);
-                    }
-                }
-                
-                /* 3: Remove one or more marked nodes */
-                if (left_node.pointed_node().compare_exchange(left_node_next.linked_value(), right_node.linked_value())).is_ok() {
-                    if (right_node.value() != EMPTY_FLAG) && right_node.pointed_node().is_marked() {
-                        break;
-                    }
-                    else {
-                        return (left_node, right_node);
-                    }
-                }
-            }
-        }
-    } 
 
-    fn get_headptr_head(&self) -> (NodePtr, NodePtr) { // 两个返回值分别为&head和head
-        let mut left_node: NodePtr = NodePtr::default();
-        let mut left_node_next: NodePtr = NodePtr::default();
-        let mut right_node: NodePtr = NodePtr::default();
-        loop {
-            loop {
-                let mut t: NodePtr = NodePtr::from_value(&self.head as *const ListNode as *mut ListNode as *mut ());
-                let mut t_next: NodePtr = self.head.marked_ptr();
-    
                 /* 1: Find left_node and right_node */
                 loop {
                     if !t_next.is_marked() {
@@ -187,35 +162,93 @@ impl LinkedList {
                     t = NodePtr::from_value(t_next.unmark());
                     if t.value() == EMPTY_FLAG {
                         break;
-                    } 
+                    }
                     t_next = t.next();
                     // rust没有do-while，因此这样退出循环
-                    if !t_next.is_marked() { // if的第二个条件，将原论文的按值查找改为了按指针查找
+                    if !(t_next.is_marked() || (t.ptr() != item)) {
+                        // if的第二个条件，将原论文的按值查找改为了按指针查找
                         break;
                     }
                 }
                 right_node = t;
-    
+
                 /* 2: Check nodes are adjacent*/
                 if left_node_next.value() == right_node.value() {
                     if (right_node.value() != EMPTY_FLAG) && right_node.pointed_node().is_marked() {
                         break;
-                    }
-                    else {
+                    } else {
                         return (left_node, right_node);
                     }
                 }
-                
+
                 /* 3: Remove one or more marked nodes */
-                if (left_node.pointed_node().compare_exchange(left_node_next.linked_value(), right_node.linked_value())).is_ok() {
+                if (left_node
+                    .pointed_node()
+                    .compare_exchange(left_node_next.linked_value(), right_node.linked_value()))
+                .is_ok()
+                {
                     if (right_node.value() != EMPTY_FLAG) && right_node.pointed_node().is_marked() {
                         break;
-                    }
-                    else {
+                    } else {
                         return (left_node, right_node);
                     }
                 }
             }
         }
-    } 
+    }
+
+    fn get_headptr_head(&self) -> (NodePtr, NodePtr) {
+        // 两个返回值分别为&head和head
+        let mut left_node: NodePtr = NodePtr::default();
+        let mut left_node_next: NodePtr = NodePtr::default();
+        let mut right_node: NodePtr;
+        loop {
+            loop {
+                let mut t: NodePtr =
+                    NodePtr::from_value(&self.head as *const ListNode as *mut ListNode as *mut ());
+                let mut t_next: NodePtr = self.head.marked_ptr();
+
+                /* 1: Find left_node and right_node */
+                loop {
+                    if !t_next.is_marked() {
+                        left_node = t;
+                        left_node_next = t_next;
+                    }
+                    t = NodePtr::from_value(t_next.unmark());
+                    if t.value() == EMPTY_FLAG {
+                        break;
+                    }
+                    t_next = t.next();
+                    // rust没有do-while，因此这样退出循环
+                    if !t_next.is_marked() {
+                        // if的第二个条件，将原论文的按值查找改为了按指针查找
+                        break;
+                    }
+                }
+                right_node = t;
+
+                /* 2: Check nodes are adjacent*/
+                if left_node_next.value() == right_node.value() {
+                    if (right_node.value() != EMPTY_FLAG) && right_node.pointed_node().is_marked() {
+                        break;
+                    } else {
+                        return (left_node, right_node);
+                    }
+                }
+
+                /* 3: Remove one or more marked nodes */
+                if (left_node
+                    .pointed_node()
+                    .compare_exchange(left_node_next.linked_value(), right_node.linked_value()))
+                .is_ok()
+                {
+                    if (right_node.value() != EMPTY_FLAG) && right_node.pointed_node().is_marked() {
+                        break;
+                    } else {
+                        return (left_node, right_node);
+                    }
+                }
+            }
+        }
+    }
 }
